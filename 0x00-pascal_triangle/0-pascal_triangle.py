a30e68b32a588a0f292e1db0d43bc428600724b9
Malika <malikaoubilla@gmail.com>
2024-04-25 18:19:32 +0100

    pycodesstyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,7 +3,11 @@
 0-pascal_triangle.py
 """
 
+
 def pascal_triangle(n):
+    """
+    function that shows Pascal triangle rows
+    """
     if n <= 0:
         return []
     else:
@@ -11,10 +15,9 @@ def pascal_triangle(n):
         # i is the row index, j is the column index
         for i in range(1, n):
             row = []
-            for j in range (len(triangle[i - 1]) + 1):
+            for j in range(len(triangle[i - 1]) + 1):
                 value1 = triangle[i - 1][j - 1] if j - 1 >= 0 else 0
                 value2 = triangle[i - 1][j] if j < len(triangle[i - 1]) else 0
                 row.append(value1 + value2)
             triangle.append(row)
         return triangle
-            
