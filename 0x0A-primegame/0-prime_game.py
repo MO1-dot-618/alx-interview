522c78869558eeb372d428b9b49a462a5da6cfe2
Malika <65332320+MO1-dot-618@users.noreply.github.com>
2024-07-01 09:15:32 +0100

    0-prime_game.py

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Module for Prime Game"""
+
+def get_primes(numbers):
+    def is_prime(n):
+        if n < 2:
+            return False
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+    
+    return [num for num in numbers if is_prime(num)]
+
+
+def isWinner(x, nums):
+    """
+    Determines the winner of a set of prime number removal games.
+
+    Args:
+        x (int): The number of rounds.
+        nums (list of int): A list of integers where each integer n denotes
+        a set of consecutive integers starting from 1 up to and including n.
+
+    Returns:
+        str: The name of the player who won the most rounds (either "Ben"
+        or "Maria").
+        None: If the winner cannot be determined.
+
+    Raises:
+        None.
+    """
+
+    if x <= 0 or nums is None or x != len(nums):
+        return None
+
+    maria = 0
+    ben = 0
+
+    for num in nums:
+        if len(get_primes(range(num) + 1)) % 2 == 0:
+            ben++
+        else:
+            maira++
+
+    if maria > ben:
+        return "Maria"
+    elif ben > maria:
+        return "Ben"
+    else:
+        return None
+    
+
+
+
